b4aec51084c518227f9c0c4076ef1b34566bbab1
Keita <herausforderer09@gmail.com>
2026-04-01 15:16:39 +0900

    initial commit

---
+++ src/main/java/com/example/nplusone_join_sample/NplusoneJoinSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.nplusone_join_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NplusoneJoinSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NplusoneJoinSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nplusone_join_sample/NplusoneJoinSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nplusone_join_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NplusoneJoinSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
